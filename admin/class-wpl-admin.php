2a7ee76d7fe687f41d8dc6d05e28b57b4630b287
riddhi <riddhi.multidots@gmail.com>
2018-09-29 12:27:34 +0530

    added

--- admin/class-wpl-admin.php
+++ admin/class-wpl-admin.php
@@ -9,4 +9,9 @@ class wpl_admin_class {
 	function __construct() {
 		add_action( 'admin_menu', array( $this, 'wpl_admin_form' ) );
 	}	
+
+
+	public function wpl_admin_form(){
+		
+	}
 }
--- wp-library.php
+++ wp-library.php
@@ -15,8 +15,12 @@ register_activation_hook( __FILE__, 'wpl_activation_fun' );
 function wpl_activation_fun() {
 }
 
+register_deactivation_hook( __FILE__, 'wpl_deactivation_fun' );
+function wpl_deactivation_fun() {
+}
+
 
 if ( is_admin() ) {
-	//require_once( wp_survey_form_path . '/admin/class-wp-survey-form-admin.php' );
-	//new wp_survey_form_admin();
+	require_once( wpl_plugin_path . '/admin/class-wpl-admin.php' );
+	new wpl_admin_class();
 }
