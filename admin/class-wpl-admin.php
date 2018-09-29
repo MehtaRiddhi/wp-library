10c5426afa6951aae4e27e53b464d9b691ad775f
riddhi <riddhi.multidots@gmail.com>
2018-09-29 18:16:10 +0530

    added

--- admin/class-wpl-admin.php
+++ admin/class-wpl-admin.php
@@ -1,5 +1,7 @@
 <?php
+
 class wpl_admin_class {
+
 	/**
 	 * Initializes WordPress hooks
 	 *
@@ -7,11 +9,84 @@ class wpl_admin_class {
 	 * Since: 1.0.0
 	 */
 	function __construct() {
-		add_action( 'admin_menu', array( $this, 'wpl_admin_form' ) );
+		add_action( 'add_meta_boxes', array( $this, 'wpl_mata_box' ) );
+		add_action( 'save_post',      array( $this, 'save_posttitle' ) );
 	}	
 
 
-	public function wpl_admin_form(){
-		
+	/**
+	 * function for custom mata box to the custom posttype
+	 * @author Riddhi
+	 * @since 1.0.0
+	 * @param array screens
+	 * 
+	 */
+	public function wpl_mata_box() {
+
+		$screens = array( 'product' );
+		foreach ( $screens as $screen ) {
+			add_meta_box( "wpl-library", esc_html_e( 'Custom Mata Box', 'wp-library' ), "custommatabox", $screen, "side", "low" );
+		}
+
+		/**
+		 * function for custom mata box seetings HTML
+		 * 
+		 * @since 1.0.0
+		 * 
+		 */
+		function custommatabox() {
+
+			global $post;
+			ob_start();
+			$get_title = get_post_meta( $post->ID, 'text-title', true );
+		 	$get_title = !empty($get_title) ? $get_title : '';
+			?>
+			<table width="100%">
+				<?php 
+				do_action('wpl_mata_box_html_before',$post);
+				?>
+				<tr>
+					<td><?php esc_html_e( 'Post Title', 'wp-library' ); ?></td>
+					<td>
+						<input type="text" name="add-title" value="<?php esc_attr($get_title); ?>" style="width:100%;"/>
+					</td>
+				</tr>
+				<?php do_action('wpl_mata_box_html_after',$post);?>
+
+			</table>
+	
+		<?php
+		}
+
+		$out_put = ob_get_clean();
+
+		/**
+		 * Filter for Metabox HTML.
+		 * 
+		 * @param string $out_put
+		 * 
+		 * @return string $out_put
+		 */
+		$out_put = apply_filters('wpl_mata_box_html',$out_put, $post);
+
+		echo $out_put;
+
 	}
+
+	/**
+	 * function for save custom mata box settings
+	 * 
+	 * @since 1.0.0
+	 * 
+	 */
+	public function save_posttitle() {
+		global $post;
+
+		if ( $post->post_type == 'product' ) {
+			$title = $_POST['add-title'];
+			update_post_meta( $post->ID, 'text-title', $title );
+		}
+
+	}
+	
 }
--- wp-library.php
+++ wp-library.php
@@ -19,6 +19,86 @@ register_deactivation_hook( __FILE__, 'wpl_deactivation_fun' );
 function wpl_deactivation_fun() {
 }
 
+// register custom post type
+add_action( 'init', 'wpl_custom_post_type_product' );
+
+/**
+ * function for ragsiter custom post type with taxonomy
+ * 
+ * @since 1.0.0
+ * 
+ * @param array register_post_type
+ * @param array register_taxonomy
+ */
+function wpl_custom_post_type_product() {
+
+	register_post_type( 'product', array(
+
+		'labels'              => array(
+			'name'               => __( 'Products' ),
+			'singular_name'      => __( 'Products' ),
+			'add_new'            => __( 'Add New'),
+			'add_new_item'       => __( 'Create New Products'),
+			'edit'               => __( 'Edit' ),
+			'edit_item'          => __( 'Edit Products'),
+			'new_item'           => __( 'New Products'),
+			'view'               => __( 'View Products'),
+			'view_item'          => __( 'View Products'),
+			'search_items'       => __( 'Search Products'),
+			'not_found'          => __( 'No Products found'),
+			'not_found_in_trash' => __( 'No Products found in trash'),
+			'parent'             => __( 'Parent Products'),
+		),
+		'description'         => __( 'This is custom post type add to your site.'),
+		'public'              => true,
+		'show_ui'             => true,
+		'has_archive'         => true,
+		'capability_type'     => 'post',
+		'publicly_queryable'  => true,
+		'exclude_from_search' => false,
+		'menu_position'       => 5,
+		'hierarchical'        => true,
+		'query_var'           => true,
+		'supports'            => array(
+			'title',
+			'editor',
+			'author',
+			'thumbnail',
+			'excerpt',
+			'trackbacks',
+			'custom-fields',
+			'comments',
+			'revisions',
+			'sticky',
+			'page-attributes',
+			'menu_order'
+		),
+	) );
+
+	register_taxonomy( 'product-type', array( 'product' ), array(
+		'hierarchical'          => true,
+		'labels'                => array(
+			'name'              => __( 'Products Type'),
+			'singular_name'     => __( 'Products Type'),
+			'search_items'      => __( 'Search Products Types'),
+			'all_items'         => __( 'All Products Types'),
+			'parent_item'       => __( 'Parent Products Type'),
+			'parent_item_colon' => __( 'Parent Products Type:'),
+			'edit_item'         => __( 'Edit Products Type'),
+			'update_item'       => __( 'Update Products Type'),
+			'add_new_item'      => __( 'Add New Products Type'),
+			'new_item_name'     => __( 'New Blogs Products Name'),
+
+		),
+		'show_ui'               => true,
+		'query_var'             => true,
+		'order'                 => true,
+		'update_count_callback' => '_update_post_term_count',
+		'rewrite'               => array( 'with_front' => true, 'hierarchical' => true ),
+		)
+	);
+
+}
 
 if ( is_admin() ) {
 	require_once( wpl_plugin_path . '/admin/class-wpl-admin.php' );
